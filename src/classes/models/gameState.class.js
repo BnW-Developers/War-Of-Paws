8df48dd04cf8a2a47935e030d168f8e16a88c41f
Minu Han <mwj1205@naver.com>
2024-11-16 17:28:32 +0900

    feat: User, GameState 클래스 스켈레톤 추가
    - 연결된 유저의 기본 정보를 담는 User 클래스 추가
    - 게임 내부에서 다루는 유저의 정보를 담는 GameState 클래스 추가
    
    ref: #3

---
+++ src/classes/models/gameState.class.js
@@ -0,0 +1,18 @@
+// 유저의 게임 데이터를 담는 클래스
+class GameState {
+  constructor(userInstance) {
+    this.userId = userInstance.userId;
+    this.socket = userInstance.socket;
+
+    // TODO: 데이터 테이블에서 가져오도록 수정
+    // 기본 상태 하드코딩
+    this.minerals = 100;
+    this.mineralRate = 1;
+    this.buildings = [];
+    this.units = [];
+    this.baseHp = 1000;
+    this.capturedPoints = [];
+  }
+}
+
+export default GameState;
---
+++ src/classes/models/user.class.js
@@ -0,0 +1,23 @@
+class User {
+  constructor(socket, userId) {
+    this.socket = socket;
+    this.userId = userId;
+    this.sequence = 0;
+    this.matchMaking = false;
+    this.currentGameId = null; // 유저가 참가한 게임 세션 id
+  }
+
+  getSocket() {
+    return this.socket;
+  }
+
+  getUserId() {
+    return this.userId;
+  }
+
+  getNextSequence() {
+    return ++this.sequence;
+  }
+}
+
+export default User;
