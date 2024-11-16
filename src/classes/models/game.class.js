f1540814b29050fe5eb99711e04f3acf2ef0cb7b
Minu Han <mwj1205@naver.com>
2024-11-16 18:16:40 +0900

    feat: Game 클래스 추가
    - 게임의 현재 상태를 저장할 Game 클래스 생성
    
    ref #3

---
+++ src/classes/models/game.class.js
@@ -0,0 +1,60 @@
+import GameState from './gameState.class.js';
+
+const MAX_PLAYERS = 2;
+
+class Game {
+  constructor(gameId) {
+    this.gameId = gameId;
+    this.players = new Map();
+    this.state = 'waiting';
+  }
+
+  getGameId() {
+    return this.gameId;
+  }
+
+  addUser(user) {
+    if (this.players.size >= MAX_PLAYERS) {
+      throw new Error('Game is full');
+    }
+
+    const gameState = new GameState(user);
+    this.players.set(user.userId, gameState);
+    user.setCurrentGameId(this.gameId);
+
+    // TODO: redis에 게임 상태 저장
+
+    if (this.players.size >= MAX_PLAYERS) {
+      this.startGame();
+    }
+  }
+
+  // userId로 게임 세션에서 유저 검색
+  getUserByUserId(userId) {
+    return this.players.get(userId);
+  }
+
+  // userId로 게임 세션의 다른 유저 검색
+  getOpponentUserByUserId(userId) {
+    // Map에서 자신(userId)을 제외한 다른 유저를 반환
+    for (const [key, value] of this.players.entries()) {
+      if (key !== userId) {
+        return value; // GameState 객체 반환
+      }
+    }
+    return null; // 상대방이 없는 경우
+  }
+
+  removeUser(userId) {
+    // userId에 해당하는 세션이 있으면 삭제하고 성공 여부 반환
+    return this.players.delete(userId);
+  }
+
+  startGame() {
+    this.state = 'playing';
+
+    // TODO: 매치 완료 패킷 전송
+  }
+}
+
+export default Game;
