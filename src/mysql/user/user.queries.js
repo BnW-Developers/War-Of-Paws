fc67aa3cf1c7df638d4d6bbfffc9868ff84ec243
Minu Han <mwj1205@naver.com>
2024-11-18 10:21:32 +0900

    fix: mysql 쿼리문 수정
    - 현재 db에 맞게 쿼리문 수정

--- src/mysql/user/user.queries.js
+++ src/mysql/user/user.queries.js
@@ -1,4 +1,4 @@
 export const SQL_QUERIES = {
-  FIND_USER_BY_ID: 'SELECT * FROM accounts WHERE id = ?',
-  CREATE_USER: `INSERT INTO accounts (id, password, email, created_at) VALUES(?, ?, ?, ?)`,
+  FIND_USER_BY_ID: 'SELECT * FROM User WHERE user_id = ?',
+  CREATE_USER: `INSERT INTO User (user_id, password, email, created_at) VALUES(?, ?, ?, ?)`,
 };
