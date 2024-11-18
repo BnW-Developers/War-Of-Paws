7b47f4b0e3b5aabfaa2e1885471036cda10c29a3
Minu Han <mwj1205@naver.com>
2024-11-18 18:55:28 +0900

    fix: CREATE_USER 쿼리문 수정
    - create_at 인데 create'd'_at 으로 적혀있던 오타 수정
    
    Ref: #4

--- src/mysql/user/user.queries.js
+++ src/mysql/user/user.queries.js
@@ -1,4 +1,4 @@
 export const SQL_QUERIES = {
   FIND_USER_BY_ID: 'SELECT * FROM User WHERE user_id = ?',
-  CREATE_USER: `INSERT INTO User (user_id, password, email, created_at) VALUES(?, ?, ?, ?)`,
+  CREATE_USER: `INSERT INTO User (user_id, password, email, create_at) VALUES(?, ?, ?, ?)`,
 };
