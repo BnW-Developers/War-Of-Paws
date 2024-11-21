bd0578a08c231b32d4f0314a695c92026ce42707
Minu Han <mwj1205@naver.com>
2024-11-21 12:50:07 +0900

    feat: 게임 시작 관련 상수 추가
    - GAME_START_TIMEOUT: 유저들의 gameStartRequest를 기다리는 최대 시간
    - GAME_START_REQUEST_REQUIRE: 게임 시작을 위해 필요한 플레이어 수
    
    Ref: #7

--- src/constants/game.constants.js
+++ src/constants/game.constants.js
@@ -1,5 +1,7 @@
 export const GAME_CONSTANTS = {
   MAX_PLAYERS: 2,
+  GAME_START_TIMEOUT: 30 * 1000, // gameStartRequest를 30초동안 기다림
+  GAME_START_REQUEST_REQUIRE: 2, // 게임 시작을 위해 필요한 플레이어 수
 };
 
 Object.freeze(GAME_CONSTANTS);
