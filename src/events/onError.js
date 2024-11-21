1a4159203a175b1c27e0b475896424dc03512ed8
Minu Han <mwj1205@naver.com>
2024-11-21 22:40:43 +0900

    feat: onEnd, onError 로직 추가
    - 유저가 매칭 중이라면 큐에서 유저 삭제
    - 유저가 게임 중이라면 처리할 로직 추가 필요
    - 유저 세션 매니저에서 해당 유저 삭제
    
    Ref: #55

--- src/events/onError.js
+++ src/events/onError.js
@@ -1,8 +1,29 @@
+import userSessionManager from '../classes/managers/userSessionManager.js';
+import matchingSystem from '../matchmakingQueue/matchingSystem.js';
 import CustomErr from '../utils/error/customErr.js';
 import { errCodes } from '../utils/error/errCodes.js';
 import { handleErr } from '../utils/error/handlerErr.js';
+import logger from '../utils/logger.js';
 
 export const onError = (socket) => (err) => {
-  console.error('Socket Error:', err?.message);
-  handleErr(socket, new CustomErr(errCodes.SOCKET_ERR, `Socket Error: ${err.message}`));
+  try {
+    logger.error(`Socket Error:, ${err?.message}`);
+    const user = userSessionManager.getUserBySocket(socket);
+    if (user) {
+      // 유저 세션에서 해당 유저 삭제
+      // 유저가 매칭을 돌리고 있을 때 접속을 종료했다면
+      if (user.getIsMatchmaking()) {
+        // 매칭 큐에서 유저 삭제
+        matchingSystem.removeUser(user.getUserId(), user.getCurrentSpecies());
+      }
+
+      if (user.getCurrentGameId()) {
+        // TODO 게임 중 접속 종료 시 처리 추가
+      }
+    }
+    userSessionManager.removeUser(user.getUserId());
+    handleErr(socket, new CustomErr(errCodes.SOCKET_ERR, `Socket Error: ${err.message}`));
+  } catch (err) {
+    handleErr(null, err);
+  }
 };
