d689c71d4c721115c5d12e67ae48a6b97fc0e2db
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-22 01:43:54 +0900

    style: 위치 동기화 매니저의 메서드 순서를 호출 흐름순으로 재정렬
    Ref: #15

--- src/classes/managers/locationSyncManager.js
+++ src/classes/managers/locationSyncManager.js
@@ -16,6 +16,69 @@ class LocationSyncManager {
   }
 
   /**
+   * 두 좌표가 같은 위치인지 비교하여 결과를 반환
+   * @param {{x: float, y: float, z: float}} pos1
+   * @param {{x: float, y: float, z: float}} pos2
+   * @returns {boolean}
+   */
+  comparePosition(pos1, pos2) {
+    // 검증: 좌표의 형식이 올바른가?
+    if (!pos1 || !pos1.x || !pos1.y || !pos1.z) {
+      throw new Error('잘못된 좌표입니다: pos1', pos1);
+    }
+
+    if (!pos2 || !pos2.x || !pos2.y || !pos2.z) {
+      throw new Error('잘못된 좌표입니다: pos1', pos2);
+    }
+
+    return pos1.x === pos2.x && pos1.y === pos2.y && pos1.z === pos2.z;
+  }
+
+  /**
+   * 클라이언트가 보낸 유닛의 위치값을 보정하여 보정된 위치값을 반환
+   * @param {[x: float, y: float, z: float]} actualPos
+   * @param {[x: float, y: float, z: float]} expectedPos
+   * @param {[x: float, y: float, z: float]} marginOfErr
+   * @return {{adjustedPosition: {x: float, y: float, z: float}, modified: boolean}}
+   */
+  adjustPosition(actualPos, expectedPos, marginOfErr) {
+    // 검증: 좌표의 형식이 올바른가?
+    if (!actualPos || actualPos.length != 3) {
+      throw new Error('잘못된 좌표입니다: actualPos', actualPos);
+    }
+    if (!expectedPos || expectedPos.length != 3) {
+      throw new Error('잘못된 좌표입니다: expectedPos', expectedPos);
+    }
+    if (!marginOfErr || marginOfErr.length != 3) {
+      throw new Error('잘못된 좌표입니다: marginOfErr', marginOfErr);
+    }
+
+    const adjustedPos = [];
+    let modified = false;
+
+    for (let dir = 0; dir < 3; dir++) {
+      const withinLowerBound = expectedPos[dir] - marginOfErr[dir] <= actualPos[dir];
+      const withinUpperBound = actualPos[dir] <= expectedPos[dir] + marginOfErr[dir];
+      const validPosition = withinLowerBound && withinUpperBound;
+
+      // 오차범위 내일 경우 클라이언트가 보낸 위치값을 실값으로 인정
+      if (validPosition) {
+        adjustedPos[dir] = actualPos[dir];
+        // 오차범위 밖일 경우 서버의 계산값을 실값으로 처리
+      } else {
+        adjustedPos[dir] = expectedPos[dir];
+        modified = true; // 해당 좌표가 보정되었는지 여부
+      }
+    }
+
+    // 배열 -> 객체로 형식을 변경한 뒤 반환
+    return {
+      adjustedPosition: { x: adjustedPos[0], y: adjustedPos[1], z: adjustedPos[2] },
+      modified,
+    };
+  }
+
+  /**
    * 동기화에 사용될 위치값 업데이트
    * @param {number} gameId
    * @param {string} userId
@@ -122,69 +185,6 @@ class LocationSyncManager {
    */
   resetSyncPositions(gameId) {
     this.positionsToSync.set(gameId, new Map());
-  }
-
-  /**
-   * 두 좌표가 같은 위치인지 비교하여 결과를 반환
-   * @param {{x: float, y: float, z: float}} pos1
-   * @param {{x: float, y: float, z: float}} pos2
-   * @returns {boolean}
-   */
-  comparePosition(pos1, pos2) {
-    // 검증: 좌표의 형식이 올바른가?
-    if (!pos1 || !pos1.x || !pos1.y || !pos1.z) {
-      throw new Error('잘못된 좌표입니다: pos1', pos1);
-    }
-
-    if (!pos2 || !pos2.x || !pos2.y || !pos2.z) {
-      throw new Error('잘못된 좌표입니다: pos1', pos2);
-    }
-
-    return pos1.x === pos2.x && pos1.y === pos2.y && pos1.z === pos2.z;
-  }
-
-  /**
-   * 클라이언트가 보낸 유닛의 위치값을 보정하여 보정된 위치값을 반환
-   * @param {[x: float, y: float, z: float]} actualPos
-   * @param {[x: float, y: float, z: float]} expectedPos
-   * @param {[x: float, y: float, z: float]} marginOfErr
-   * @return {{adjustedPosition: {x: float, y: float, z: float}, modified: boolean}}
-   */
-  adjustPosition(actualPos, expectedPos, marginOfErr) {
-    // 검증: 좌표의 형식이 올바른가?
-    if (!actualPos || actualPos.length != 3) {
-      throw new Error('잘못된 좌표입니다: actualPos', actualPos);
-    }
-    if (!expectedPos || expectedPos.length != 3) {
-      throw new Error('잘못된 좌표입니다: expectedPos', expectedPos);
-    }
-    if (!marginOfErr || marginOfErr.length != 3) {
-      throw new Error('잘못된 좌표입니다: marginOfErr', marginOfErr);
-    }
-
-    const adjustedPos = [];
-    let modified = false;
-
-    for (let dir = 0; dir < 3; dir++) {
-      const withinLowerBound = expectedPos[dir] - marginOfErr[dir] <= actualPos[dir];
-      const withinUpperBound = actualPos[dir] <= expectedPos[dir] + marginOfErr[dir];
-      const validPosition = withinLowerBound && withinUpperBound;
-
-      // 오차범위 내일 경우 클라이언트가 보낸 위치값을 실값으로 인정
-      if (validPosition) {
-        adjustedPos[dir] = actualPos[dir];
-        // 오차범위 밖일 경우 서버의 계산값을 실값으로 처리
-      } else {
-        adjustedPos[dir] = expectedPos[dir];
-        modified = true; // 해당 좌표가 보정되었는지 여부
-      }
-    }
-
-    // 배열 -> 객체로 형식을 변경한 뒤 반환
-    return {
-      adjustedPosition: { x: adjustedPos[0], y: adjustedPos[1], z: adjustedPos[2] },
-      modified,
-    };
   }
 }
 
