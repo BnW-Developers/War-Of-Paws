5ba27c3016694266b09ba4307f6fa000fe0f4664
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-22 00:46:27 +0900

    refactor: 위치 동기화 핸들러의 검증코드를 getSessionInfo로 대체
    Ref: #15

--- src/handlers/sync/locationNotification.js
+++ src/handlers/sync/locationNotification.js
@@ -1,9 +1,8 @@
 import { handleErr } from '../../utils/error/handlerErr.js';
-import gameSessionManager from '../../classes/managers/gameSessionManager.js';
 import { errCodes } from '../../utils/error/errCodes.js';
 import CustomErr from '../../utils/error/customErr.js';
-import userSessionManager from '../../classes/managers/userSessionManager.js';
 import locationSyncManager from '../../classes/managers/locationSyncManager.js';
+import checkSessionInfo from '../../utils/sessions/checkSessionInfo.js';
 
 /**
  * **위치 동기화 핸들러**
@@ -26,33 +25,7 @@ import locationSyncManager from '../../classes/managers/locationSyncManager.js';
  */
 const locationNotification = (socket, payload) => {
   try {
-    // 검증: 유저가 존재하는가?
-    const user = userSessionManager.getUserBySocket(socket);
-    if (!user) {
-      throw new CustomErr(errCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다.');
-    }
-    const userId = user.getUserId();
-
-    // 검증: 유저가 게임에 참가했는가?
-    const gameSession = gameSessionManager.getGameSessionBySocket(socket);
-    if (!gameSession) {
-      throw new CustomErr(errCodes.GAME_NOT_FOUND, '유저가 플레이중인 게임을 찾지 못했습니다.');
-    }
-    const gameId = gameSession.getGameId();
-
-    // 검증: 플레이어 데이터가 존재하는가?
-    const playerGameData = gameSession.getPlayerGameData(userId);
-    if (!playerGameData) {
-      throw new CustomErr(
-        errCodes.PLAYER_GAME_DATA_NOT_FOUND,
-        '유저의 게임 데이터를 찾지 못했습니다.',
-      );
-    }
-
-    // 검증: 게임이 진행중인가?
-    if (!gameSession.isInProgress()) {
-      throw new CustomErr(errCodes.GAME_NOT_IN_PROGRESS, `진행중인 게임이 아닙니다.`);
-    }
+    const { gameId, userId, userGameData, opponentId, opponentSocket } = checkSessionInfo(socket);
 
     // 해당 클라이언트가 보유한 유닛들의 위치
     const { unitPositions } = payload;
@@ -64,7 +37,7 @@ const locationNotification = (socket, payload) => {
       const { unitId, position } = unitPosition;
 
       // 검증: 해당 플레이어가 보유한 (소환한) 유닛인가?
-      const unit = playerGameData.getUnit(unitId);
+      const unit = userGameData.getUnit(unitId);
       if (!unit) {
         throw new CustomErr(errCodes.UNOWNED_UNIT, '유저가 보유한 유닛이 아닙니다.');
       }
@@ -90,22 +63,6 @@ const locationNotification = (socket, payload) => {
 
     // 양 플레이어로부터 위치패킷을 받고 처리했다면 (모든 유닛의 동기화 위치값이 산출되었다면) 위치 동기화 실행
     if (locationSyncManager.isSyncReady(gameId)) {
-      // 검증: 상대방 유저의 플레이어 데이터가 존재하는가?
-      const opponentGameData = gameSession.getOpponentGameDataByUserId(userId);
-      if (!opponentGameData) {
-        throw new CustomErr(
-          errCodes.OPPONENT_GAME_DATA_NOT_FOUND,
-          '상대방의 게임 데이터를 찾을 수 없습니다',
-        );
-      }
-      const opponentId = opponentGameData.getUserId();
-
-      // 검증: 상대방의 소켓이 존재하는가?
-      const opponentSocket = opponentGameData.getSocket();
-      if (!opponentSocket) {
-        throw new CustomErr(errCodes.OPPONENT_SOCKET_NOT_FOUND, '상대방의 소켓을 찾을 수 없습니다');
-      }
-
       // 위치동기화 실행
       locationSyncManager.syncPositions(gameId, userId, opponentId, socket, opponentSocket);
     }
