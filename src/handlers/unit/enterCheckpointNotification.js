b55391d390c787accde32f9706a735e7fdbe1971
tjdcjf1996 <r_louis@naver.com>
2024-11-22 16:05:57 +0900

    fix: 누락 부분 추가
    체크포인트 매니저 불러오기 누락으로 인한 추가
    
    Ref: #13

--- src/handlers/unit/enterCheckpointNotification.js
+++ src/handlers/unit/enterCheckpointNotification.js
@@ -1,11 +1,23 @@
+import CustomErr from '../../utils/error/customErr.js';
+import { ERR_CODES } from '../../utils/error/errCodes.js';
+import { handleErr } from '../../utils/error/handlerErr.js';
 import checkSessionInfo from '../../utils/sessions/checkSessionInfo.js';
 
 const enterCheckpointNotification = (socket, payload) => {
   const { isTop, unitId } = payload;
 
-  const { gameSession } = checkSessionInfo(socket);
-  //메서드 실행
-  gameSession.addUnit(isTop, unitId);
+  try {
+    const { gameSession } = checkSessionInfo(socket);
+
+    const CheckPointManager = gameSession.getCheckPointManager();
+    if (!CheckPointManager) {
+      throw new CustomErr(ERR_CODES.INVALID_GAME_STATE, 'CheckPointManager is not found');
+    }
+    //메서드 실행
+    CheckPointManager.addUnit(isTop, unitId);
+  } catch (err) {
+    handleErr(socket, err);
+  }
 };
 
 export default enterCheckpointNotification;
