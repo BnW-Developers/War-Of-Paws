f06ad987723fd213f8f3d0d0f886b4dd2ed855e0
tjdcjf1996 <r_louis@naver.com>
2024-11-22 15:43:29 +0900

    refactor: checkSessionInfo 메서드를 통한 리팩터링
    Ref: #13

--- src/handlers/unit/enterCheckpointNotification.js
+++ src/handlers/unit/enterCheckpointNotification.js
@@ -1,29 +1,11 @@
-import userSessionManager from '../../classes/managers/userSessionManager.js';
-import gameSessionManager from './../../classes/managers/gameSessionManager.js';
+import checkSessionInfo from '../../utils/sessions/checkSessionInfo.js';
 
 const enterCheckpointNotification = (socket, payload) => {
   const { isTop, unitId } = payload;
 
-  // 유저 불러오기
-  const user = userSessionManager.getUserBySocket(socket);
-  if (!user) {
-    throw new Error('User not found');
-  }
-  const gameId = user.getCurrentGameId();
-
-  // 게임세션 불러오기
-  const game = gameSessionManager.getGameByGameId(gameId);
-  if (!game) {
-    throw new Error('Game not found');
-  }
-  // 체크포인트 매니저 불러오기
-  const CheckPointManager = game.getCheckPointManager();
-  if (!CheckPointManager) {
-    throw new Error('CheckPointManager not found');
-  }
-
+  const { gameSession } = checkSessionInfo(socket);
   //메서드 실행
-  CheckPointManager.addUnit(isTop, unitId);
+  gameSession.addUnit(isTop, unitId);
 };
 
 export default enterCheckpointNotification;
