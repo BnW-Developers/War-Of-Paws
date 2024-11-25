dea178c396950611372cbc313131e51ee20dc0cb
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-25 09:22:08 +0900

    refactor: 유닛 프로퍼티 용도별로 분류 및 정리
    Ref: #10, #11, #12, #15

--- src/classes/models/unit.class.js
+++ src/classes/models/unit.class.js
@@ -2,19 +2,23 @@ class Unit {
   static idCounter = 1;
 
   constructor(unitData, toTop) {
+    // ID 및 종족 관련
     this.assetId = unitData.id;
+    this.unitId = Unit.idCounter++;
+
+    // 능력치 관련
     this.maxHp = unitData.maxHp;
     this.hp = unitData.maxHp;
     this.attackPower = unitData.atk;
     this.def = unitData.def;
     this.speed = unitData.spd;
     this.cooldown = unitData.cd;
+
+    // 코스트 관련
     this.cost = unitData.cost;
 
-    // toTop에 의한 초기 위치 설정
+    // 이동 관련
     this.position = toTop ? { x: 0, y: 0, z: 10 } : { x: 0, y: 0, z: -10 };
-
-    this.unitId = Unit.idCounter++;
   }
 
   getUnitId() {
