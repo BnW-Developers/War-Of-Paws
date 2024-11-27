edc97205e308a5684d0d06a9cdd281ce9ba8934a
Minu Han <mwj1205@naver.com>
2024-11-27 17:53:50 +0900

    fix: game_db 마이그레이션 쿼리문 수정
    - user_id에 UNIQUE 속성 제거
    
    Ref: #8

--- src/mysql/sql/game_db.sql
+++ src/mysql/sql/game_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Game_Log (
     game_id         INT AUTO_INCREMENT PRIMARY KEY,
-    win_user_id     VARCHAR(36) UNIQUE NOT NULL,
-    lose_user_id    VARCHAR(36) UNIQUE NOT NULL,    
+    win_user_id     VARCHAR(36) NOT NULL,
+    lose_user_id    VARCHAR(36) NOT NULL,    
     create_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
