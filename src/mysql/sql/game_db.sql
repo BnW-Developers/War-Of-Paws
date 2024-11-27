fc3c217fdb31f4fc8d4221c087072b748374bac1
Minu Han <mwj1205@naver.com>
2024-11-27 17:07:28 +0900

    feat: game_db pool 생성, 마이그레이션 추가
    - db에 game_db 스키마에 새로운 테이블이 추가되도록 sql 작성
    - 서버에서 game_db에 접근할 수 있도록 풀 생성
    
    Ref: #8

---
+++ src/mysql/sql/game_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Game_Log (
+    game_id         INT AUTO_INCREMENT PRIMARY KEY,
+    win_user_id     VARCHAR(36) UNIQUE NOT NULL,
+    lose_user_id    VARCHAR(36) UNIQUE NOT NULL,    
+    create_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
