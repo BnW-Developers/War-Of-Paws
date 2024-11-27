905982cde2f12efc4b77667e476be58cf5ab416d
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-27 21:48:45 +0900

    fix: `arrivedAtDest` 로직 오류 수정
    Ref: #15

--- src/utils/location/arrivedAtDest.js
+++ src/utils/location/arrivedAtDest.js
@@ -8,9 +8,9 @@ const arrivedAtDest = (unit, pos) => {
   const { area } = unit.getDestination();
 
   const arrived =
-    pos.x >= area[0].x || // 서쪽 변 체크
-    pos.z <= area[0].z || // 북쪽 변 체크
-    pos.x <= area[2].x || // 동쪽 변 체크
+    pos.x >= area[0].x && // 서쪽 변 체크
+    pos.z <= area[0].z && // 북쪽 변 체크
+    pos.x <= area[2].x && // 동쪽 변 체크
     pos.z >= area[2].z; // 남쪽 변 체크
 
   return arrived;
