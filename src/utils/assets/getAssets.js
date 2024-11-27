11f0daf4aa339ab979659bf5a91d0478b5d0bdad
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-27 12:07:38 +0900

    comment: 애셋조회 모듈 설명 보완
    Ref: #36

--- src/utils/assets/getAssets.js
+++ src/utils/assets/getAssets.js
@@ -43,10 +43,10 @@ export const getGameAsset = (assetType) => {
 /**
  * 게임에셋의 특정 데이터를 id로 조회하는 함수
  *
- * 호출 예시: const unitData = getGameAssetById(ASSET_TYPE.UNIT, 2001);
+ * 호출 예시: const unitData = getGameAssetById(ASSET_TYPE.UNIT, 2003);
  * @param {ASSET_TYPE} assetType 조회할 게임에셋 타입
  * @param {string} id 조회할 항목의 id
- * @returns {JSON} 해당 id의 데이터 ( 예시: { id: 2003, DisplayName: "마법사", ... } )
+ * @returns {JSON} 해당 id의 데이터 ( 예시: { id: 2003, DisplayName: "불 테리어", ... } )
  */
 export const getGameAssetById = (assetType, id) => {
   const { buildings, maps, paths, units } = getAllGameAssets();
@@ -70,10 +70,12 @@ export const getGameAssetById = (assetType, id) => {
 };
 
 /**
- * 진영(멍? 냥?)과 방향에 부합하는 경로를 반환
- * @param {SPECIES} species
- * @param {DIRECTION} direction
- * @returns {{x: float, y: float, z: float}[]}
+ * 진영과 방향에 부합하는 경로를 반환
+ *
+ * 호출 예시: const pathData = getPath(SPECIES.DOG, DIRECTION.UP);
+ * @param {SPECIES} species 진영 (개 또는 고양이)
+ * @param {DIRECTION} direction 소환위치 (위 또는 아래)
+ * @returns {{x: float, y: float, z: float}[]} 경로
  */
 export const getPath = (species, direction) => {
   const { paths } = getGameAsset(ASSET_TYPE.PATH);
