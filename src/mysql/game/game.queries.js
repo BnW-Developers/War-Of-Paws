8a9eb3aa6d0e798fd95f1509f51a5c6eb34671ca
Minu Han <mwj1205@naver.com>
2024-11-28 11:41:03 +0900

    fix: RECORD_GAME 쿼리문 수정
    - 첫 알파벳을 대문자로 수정
    - mysql 버전에 따라 오류가 날 수도 안날 수도 있었음
    
    Ref: #8

--- src/mysql/game/game.queries.js
+++ src/mysql/game/game.queries.js
@@ -1,4 +1,4 @@
 export const SQL_GAME_QUERIES = {
   RECORD_GAME:
-    'INSERT INTO game_log (cat_user_id, dog_user_id, win_team, create_at) VALUES (?, ?, ?, ?)',
+    'INSERT INTO Game_Log (cat_user_id, dog_user_id, win_team, create_at) VALUES (?, ?, ?, ?)',
 };
