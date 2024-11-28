763703a41330a8f34446f5a657498e3d2bd82a6a
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-28 16:16:35 +0900

    feat: `adjustPos`의 로직 완성
    Ref: #15

--- src/utils/location/adjustPosition.js
+++ src/utils/location/adjustPosition.js
@@ -1,45 +1,25 @@
+import calcDist from './calcDist';
+
 /**
- * 클라이언트가 보낸 유닛의 위치값을 보정하여 보정된 위치값을 반환
- * @param {[x: float, y: float, z: float]} actualPos
- * @param {[x: float, y: float, z: float]} expectedPos
- * @param {[x: float, y: float, z: float]} marginOfErr
- * @return {{adjustedPos: {x: float, y: float, z: float}, modified: boolean}}
+ * 서버가 예측하는 유닛의 현재 위치값을 반환
+ * @param {Unit} unit 유닛
+ * @param {int32} timestamp 현재 위치동기화 시간
+ * @return {{x: float, y: float, z: float}}
  */
-const adjustPos = (actualPos, expectedPos, marginOfErr) => {
-  // 검증: 좌표의 형식이 올바른가?
-  if (!actualPos || actualPos.length != 3) {
-    throw new Error('잘못된 좌표입니다: actualPos', actualPos);
-  }
-  if (!expectedPos || expectedPos.length != 3) {
-    throw new Error('잘못된 좌표입니다: expectedPos', expectedPos);
-  }
-  if (!marginOfErr || marginOfErr.length != 3) {
-    throw new Error('잘못된 좌표입니다: marginOfErr', marginOfErr);
-  }
+const adjustPos = (unit, timestamp) => {
+  const timeTaken = timestamp - unit.getLastTimestamp();
+  const startPos = unit.getPosition();
+  const endPos = unit.getDestination().point;
 
-  const adjustedPos = [];
-  let modified = false;
+  const scalarDist = unit.getSpeed() * timeTaken; // 주어진 시간동안 유닛이 이동할 수 있는 직선거리
+  const totalScalarDist = calcDist(startPos, endPos); // 유닛의 현재 위치에서 목적지까지의 거리
+  const progressRate = scalarDist / totalScalarDist; // 유닛이 목적지까지 나아간 거리의 비율
 
-  for (let dir = 0; dir < 3; dir++) {
-    const withinLowerBound = expectedPos[dir] - marginOfErr[dir] <= actualPos[dir];
-    const withinUpperBound = actualPos[dir] <= expectedPos[dir] + marginOfErr[dir];
-    const validPosition = withinLowerBound && withinUpperBound;
+  const x = startPos.x + (endPos.x - startPos.x) * progressRate;
+  const y = startPos.y + (endPos.y - startPos.y) * progressRate;
+  const z = startPos.z + (endPos.z - startPos.z) * progressRate;
 
-    // 오차범위 내일 경우 클라이언트가 보낸 위치값을 실값으로 인정
-    if (validPosition) {
-      adjustedPos[dir] = actualPos[dir];
-      // 오차범위 밖일 경우 서버의 계산값을 실값으로 처리
-    } else {
-      adjustedPos[dir] = expectedPos[dir];
-      modified = true; // 해당 좌표가 보정되었는지 여부
-    }
-  }
-
-  // 배열 -> 객체로 형식을 변경한 뒤 반환
-  return {
-    adjustedPos: { x: adjustedPos[0], y: adjustedPos[1], z: adjustedPos[2] },
-    modified,
-  };
+  return { x, y, z };
 };
 
 export default adjustPos;
