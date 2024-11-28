ca091d0a1c483eb1db807f75c4f8ae5234db1354
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-28 20:20:25 +0900

    fix: `getGameAssetById`에 잘못된 ID 검증로직 추가
    Ref: #36

--- src/utils/assets/getAssets.js
+++ src/utils/assets/getAssets.js
@@ -51,15 +51,20 @@ export const getGameAsset = (assetType) => {
 export const getGameAssetById = (assetType, id) => {
   const { buildings, maps, paths, units } = getAllGameAssets();
 
+  let data = null;
   switch (assetType) {
     case ASSET_TYPE.BUILDING:
-      return buildings.data.find((building) => building.id === id);
+      data = buildings.data.find((building) => building.id === id);
+      break;
     case ASSET_TYPE.MAP:
-      return maps.data.find((map) => map.id === id);
+      data = maps.data.find((map) => map.id === id);
+      break;
     case ASSET_TYPE.PATH:
-      return paths.data.find((path) => path.id === id);
+      data = paths.data.find((path) => path.id === id);
+      break;
     case ASSET_TYPE.UNIT:
-      return units.data.find((unit) => unit.id === id);
+      data = units.data.find((unit) => unit.id === id);
+      break;
     default:
       throw CustomErr(
         ERR_CODES.INVALID_ASSET_TYPE,
@@ -67,6 +72,10 @@ export const getGameAssetById = (assetType, id) => {
         assetType,
       );
   }
+  if (!data) {
+    throw new CustomErr(ERR_CODES.INVALID_ASSET_ID, '올바르지 않은 게임에셋 ID입니다:', id);
+  }
+  return data;
 };
 
 /**
