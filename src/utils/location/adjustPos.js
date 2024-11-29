d243a463c288527b137b918977e267918c3abcbc
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-29 12:42:18 +0900

    fix: 시간단위 수정
    - 총 경로의 길이 = 2000
    - 가장 빠른 유닛 (`speed = 200`)이 시작지점에서 성채에 도달하는데 걸리는 시간 = 10초
    - 가장 느린 유닛 (`speed = 50`)이 시작지점에서 성채에 도달하는데 걸리는 시간 = 40초
    - 유닛의 속도: 초단위 (200 = 200/s)
    
    Ref: #15

--- src/utils/location/adjustPos.js
+++ src/utils/location/adjustPos.js
@@ -7,11 +7,11 @@ import calcDist from './calcDist.js';
  * @return {{x: float, z: float}}
  */
 const adjustPos = (unit, timestamp) => {
-  const timeTaken = timestamp - unit.getLastTimestamp();
+  const timeTaken = timestamp - unit.getLastTimestamp(); // ms
   const startPos = unit.getPosition();
   const endPos = unit.getDestination().point;
 
-  const scalarDist = unit.getSpeed() * timeTaken; // 주어진 시간동안 유닛이 이동할 수 있는 직선거리
+  const scalarDist = unit.getSpeed() * (timeTaken / 1000); // 주어진 시간동안 유닛이 이동할 수 있는 직선거리
   const totalScalarDist = calcDist(startPos, endPos); // 유닛의 현재 위치에서 목적지까지의 거리
   const progressRate = scalarDist / totalScalarDist; // 유닛이 목적지까지 나아간 거리의 비율
 
--- src/utils/location/calcSpd.js
+++ src/utils/location/calcSpd.js
@@ -5,7 +5,7 @@ import { ERR_CODES } from '../error/errCodes.js';
  * 두 좌표 사이의 직선거리를 이동한 속도를 반환
  * @param {{x: float, z: float}} pos1 위치 (전)
  * @param {{x: float, z: float}} pos2 위치 (후)
- * @param {number} timeTaken 걸린 시간 (초)
+ * @param {number} timeTaken 걸린 시간 (ms)
  * @returns {float}
  */
 const calcSpd = (pos1, pos2, timeTaken) => {
@@ -25,7 +25,7 @@ const calcSpd = (pos1, pos2, timeTaken) => {
 
   // √((x1-x2)^2 + (z1-z2)^2)
   const distance = Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.z - pos2.z) ** 2);
-  const speed = distance / timeTaken;
+  const speed = distance / (timeTaken / 1000);
   return speed;
 };
 
