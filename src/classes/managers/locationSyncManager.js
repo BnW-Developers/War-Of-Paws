2ef90173ae7e8f6077b03cf72f7a2fce82c233bb
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-29 09:17:02 +0900

    remove: `createLocationSyncPacket` 필요없는 인자 삭제
    Ref: #15

--- src/classes/managers/locationSyncManager.js
+++ src/classes/managers/locationSyncManager.js
@@ -41,11 +41,9 @@ class LocationSyncManager {
    * 각 클라이언트로 전송할 위치 동기화 패킷을 작성
    * @param {string} userId
    * @param {string} opponentId
-   * @param {net.Socket} socket
-   * @param {net.Socket} opponentSocket
    * @return {{userPacket: Buffer, opponentPacket: Buffer}}
    */
-  createLocationSyncPacket(gameId, userId, opponentId) {
+  createLocationSyncPacket(userId, opponentId) {
     // 해당 게임의 모든 동기화 위치값
     const { unitPositions: userSyncPositions } = this.positionsToSync.get(userId);
     const { unitPositions: opponentSyncPositions } = this.positionsToSync.get(opponentId);
