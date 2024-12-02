92364b7c6fa80d619af607dbb4b334b1bacb4cad
Minu Han <mwj1205@naver.com>
2024-12-02 13:52:26 +0900

    feat: 구글 로그인 db 쿼리문 추가
    - 구글 로그인 유저는 따로 저장
    
    Ref: #4

--- src/mysql/user/user.queries.js
+++ src/mysql/user/user.queries.js
@@ -1,4 +1,6 @@
 export const SQL_QUERIES = {
   FIND_USER_BY_ID: 'SELECT * FROM User WHERE user_id = ?',
   CREATE_USER: `INSERT INTO User (user_id, password, email, create_at) VALUES(?, ?, ?, ?)`,
+  FIND_USER_BY_GOOGLE_ID: 'SELECT * FROM users WHERE google_id = ?',
+  CREATE_GOOGLE_USER: 'INSERT INTO users (google_id, email, name, create_at) VALUES (?, ?, ?. ?)',
 };
