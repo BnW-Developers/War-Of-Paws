5c32b6b1040a1d545db908378a6cb13ebfc0934b
rlaguswls2 <hydrated200ml@gmail.com>
2024-12-02 13:10:23 +0900

    feat: 스킬 사거리 검증 추가
    Ref: #61

--- src/handlers/unit/buffUnitRequest.js
+++ src/handlers/unit/buffUnitRequest.js
@@ -1,8 +1,10 @@
 import { UNIT_TYPE } from '../../constants/assets.js';
+import { ATTACK_RANGE_ERROR_MARGIN } from '../../constants/game.constants.js';
 import { PACKET_TYPE } from '../../constants/header.js';
 import CustomErr from '../../utils/error/customErr.js';
 import { ERR_CODES } from '../../utils/error/errCodes.js';
 import { handleErr } from '../../utils/error/handlerErr.js';
+import calcDist from '../../utils/location/calcDist.js';
 import { sendPacket } from '../../utils/packet/packetManager.js';
 import checkSessionInfo from '../../utils/sessions/checkSessionInfo.js';
 
@@ -42,12 +44,26 @@ const buffUnitRequest = (socket, payload) => {
     // 결과 저장용 배열
     const affectedUnits = [];
 
+    // 사거리 검증
+    const buffUnitPosition = buffUnit.getPosition();
+    const effectiveRange = buffUnit.getAttackRange() + ATTACK_RANGE_ERROR_MARGIN;
+
     // 각 대상 유닛에 버프 적용
     for (const targetId of targetIds) {
       const targetUnit = userGameData.getUnit(targetId);
       if (!targetUnit) {
         // 경고인 이유: 버프 리스폰스를 받기 전에 사망할 수 있는 가능성 있음
         console.warn(`Target unit with ID ${targetId} not found`);
+        continue;
+      }
+
+      const targetUnitPosition = targetUnit.getPosition();
+      const distance = calcDist(buffUnitPosition, targetUnitPosition);
+
+      if (distance > effectiveRange) {
+        console.info(
+          `Target unit ${targetId} is out of range. Distance: ${distance}, Range: ${effectiveRange}`,
+        );
         continue;
       }
 
