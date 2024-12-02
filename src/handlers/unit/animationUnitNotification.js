5e00f2c0f36c0ad026df6fe14bfb1baf9503d180
rlaguswls2 <hydrated200ml@gmail.com>
2024-12-02 15:34:35 +0900

    feat: animation notification handler
    Ref: #104

---
+++ src/handlers/unit/animationUnitNotification.js
@@ -0,0 +1,31 @@
+import { PACKET_TYPE } from '../../constants/header.js';
+import CustomErr from '../../utils/error/customErr.js';
+import { ERR_CODES } from '../../utils/error/errCodes.js';
+import { handleErr } from '../../utils/error/handlerErr.js';
+import { sendPacket } from '../../utils/packet/packetManager.js';
+import checkSessionInfo from '../../utils/sessions/checkSessionInfo.js';
+
+const animationUnitNotification = (socket, payload) => {
+  try {
+    const { unitId, animationId } = payload;
+
+    const { userGameData, opponentSocket } = checkSessionInfo(socket);
+
+    const unit = userGameData.getUnit(unitId);
+    if (!unit) {
+      throw new CustomErr(ERR_CODES.UNIT_NOT_FOUND, `Unit with ID ${unitId} not found`);
+    }
+
+    // animation id 검증 필요 ?
+    // 서버내에 보유한 animation id중 하나인지
+
+    sendPacket(opponentSocket, PACKET_TYPE.ENEMY_UNIT_ANIMATION_NOTIFICATION, {
+      unitId,
+      animationId,
+    });
+  } catch (err) {
+    handleErr(socket, err);
+  }
+};
+
+export default animationUnitNotification;
