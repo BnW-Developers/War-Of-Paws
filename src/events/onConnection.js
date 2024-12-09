43048c161e500a43f2d43b0408d92782f8cdaa0f
tjdcjf1996 <r_louis@naver.com>
2024-12-09 20:34:04 +0900

    remove: IP 밴 기능 제거
    nginx를 통한 내부 네트워크 소켓 연결 수립과정으로 외부 아이피 차단 예정이라 IP밴 과정이 필요없다 판단하여 삭제
    
    Ref: #114

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -1,6 +1,3 @@
-import { blockList } from '../server.js';
-import { ERR_CODES } from '../utils/error/errCodes.js';
-import { handleErr } from '../utils/error/handlerErr.js';
 import { onData } from './onData.js';
 import { onEnd } from './onEnd.js';
 import { onError } from './onError.js';
@@ -9,20 +6,13 @@ import { v4 as uuidV4 } from 'uuid';
 export const onConnection = (socket) => {
   const remoteAddress = socket.remoteAddress;
 
-  if (blockList.check(remoteAddress)) {
-    console.log('Blocked connection from', remoteAddress);
-    handleErr(socket, { code: ERR_CODES.SOCKET_ERR, message: 'Your IP is banned' });
-    socket.destroy();
-    socket.end();
-  } else {
-    console.log('Allowed connection from', remoteAddress);
-    socket.buffer = Buffer.alloc(0);
-    socket.clientId = uuidV4();
-    socket.sequence = 0;
-    socket.illegalCount = 0;
+  console.log('Allowed connection from', remoteAddress);
+  socket.buffer = Buffer.alloc(0);
+  socket.clientId = uuidV4();
+  socket.sequence = 0;
+  socket.illegalCount = 0;
 
-    socket.on('data', onData(socket));
-    socket.on('end', onEnd(socket));
-    socket.on('error', onError(socket));
-  }
+  socket.on('data', onData(socket));
+  socket.on('end', onEnd(socket));
+  socket.on('error', onError(socket));
 };
