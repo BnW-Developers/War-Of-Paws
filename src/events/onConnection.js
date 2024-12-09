e395f2c64d2f6ddf5e39f975dd312048835b0c9f
tjdcjf1996 <r_louis@naver.com>
2024-12-09 22:41:21 +0900

    refactor: 소켓 연결 시 인증 타임아웃 추가
    소켓 연결 후 인증 패킷 10초안에 안보내면 소켓 연결 해제
    
    Ref: #114

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -4,15 +4,27 @@ import { onError } from './onError.js';
 import { v4 as uuidV4 } from 'uuid';
 
 export const onConnection = (socket) => {
-  const remoteAddress = socket.remoteAddress;
+  try {
+    const key = socket.remotePort;
 
-  console.log('Allowed connection from', remoteAddress);
-  socket.buffer = Buffer.alloc(0);
-  socket.clientId = uuidV4();
-  socket.sequence = 0;
-  socket.illegalCount = 0;
+    // 초기 소켓 설정
+    socket.buffer = Buffer.alloc(0);
+    socket.sequence = 0;
+    socket.isAuthenticated = false; // 인증 상태 플래그 추가
 
-  socket.on('data', onData(socket));
-  socket.on('end', onEnd(socket));
-  socket.on('error', onError(socket));
+    // 일정 시간 내에 인증 완료되지 않으면 연결 종료
+    socket.authTimeout = setTimeout(() => {
+      if (!socket.isAuthenticated) {
+        console.log(`인증 시간 초과: ${key}`);
+        socket.end();
+      }
+    }, 10 * 1000); // 10초 타임아웃
+
+    socket.on('data', onData(socket));
+    socket.on('end', onEnd(socket));
+    socket.on('error', onError(socket));
+  } catch (err) {
+    console.error('onConnection 오류', err);
+    socket.end();
+  }
 };
