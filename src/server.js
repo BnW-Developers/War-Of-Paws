656987089ec74aa883333221f5e7804c6380f234
tjdcjf1996 <r_louis@naver.com>
2024-12-09 22:39:57 +0900

    feat: 라우터 추가
    헬스서버에서 제공받는 매칭정보 받는 라우터 구현
    Ref: #114

--- src/server.js
+++ src/server.js
@@ -2,13 +2,22 @@ import net from 'net';
 import { onConnection } from './events/onConnection.js';
 import { initServer } from './init/initServer.js';
 import { config } from './config/config.js';
-
-export const blockList = new net.BlockList();
+import express from 'express';
+import sessionRouter from './routes/session.router.js';
+import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 
 const server = net.createServer(onConnection);
+const app = express();
+
+app.use(express.json());
+app.use('/', sessionRouter);
+app.use(errorHandlingMiddleware);
 
 initServer().then(() => {
   server.listen(config.server.port, config.server.host, () => {
     console.log(`SERVER ON - ${config.server.host} : ${config.server.port}`);
+  });
+  app.listen(config.server.apiPort, config.server.host, () => {
+    console.log(`API ON - ${config.server.host}:${config.server.apiPort}`);
   });
 });
