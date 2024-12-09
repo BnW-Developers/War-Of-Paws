4770e32d01b7b3c97d0f9e8130b3adf7737857f0
tjdcjf1996 <r_louis@naver.com>
2024-12-09 13:42:45 +0900

    feat: 서버 부팅 시 상태보고 인터벌 추가
    Ref: #114

--- src/init/initServer.js
+++ src/init/initServer.js
@@ -5,18 +5,20 @@ import { loadProtos } from './loadProto.js';
 import pools from './../mysql/createPool.js';
 import { loadGameAssets } from './loadAssets.js';
 import { loadBanList } from './loadBanList.js';
+import { notificationStatus } from '../utils/server/notiStatus.js';
 
 export const initServer = async () => {
+  let notification;
   try {
-    // TODO: 현재 기획중인 서버 설계에 따른 초기화 진행 필요한 함수 추가 필요
-    // DB 연결 안 되어있는 관계로 임시 주석
-    // await testAllConnections(pools);
+    await testAllConnections(pools);
+    notification = setInterval(notificationStatus, 5000);
     await loadBanList();
     await loadGameAssets();
     await loadProtos();
     await loadHandlers();
   } catch (err) {
     const date = new Date();
+    clearInterval(notification);
     console.error(`[${formatDate(date)} - FAIL] Failed to initialize server`, err);
     process.exit(1);
   }
