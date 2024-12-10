4565253fc134b5a885635eed493103fa5e598411
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-12-10 23:37:28 +0900

    refactor: 게임 상수 분류별로 정리

--- src/constants/game.constants.js
+++ src/constants/game.constants.js
@@ -1,18 +1,28 @@
-export const MAX_PLAYERS = 2;
-export const GAME_START_TIMEOUT = 30 * 1000; // gameStartRequest를 30초동안 기다림
+// 플레이어 수
 export const GAME_START_REQUEST_REQUIRE = 2; // 게임 시작을 위해 필요한 플레이어 수
+export const MAX_PLAYERS = 2;
+
+// 매칭 및 게임시작
+export const GAME_START_TIMEOUT = 30 * 1000; // gameStartRequest를 30초동안 기다림
+
+// 초기값
+export const INITIAL_BASE_HP = 1000;
 export const INITIAL_MINERAL = 200;
 export const INITIAL_MINERAL_RATE = 10; // 10원
+export const INITIAL_UNIT_ROTATION = Object.freeze({ up: 0, down: 180 });
+
+// 자원
 export const OCCUR_ONE_CHECKPOINT_MINERAL_RATE = 15;
 export const OCCUR_TWO_CHECKPOINT_MINERAL_RATE = 20;
 export const MINERAL_SYNC_INTERVAL = 2000; // 2초당
-export const INITIAL_BASE_HP = 1000;
 
-export const ATTACK_COOLDOWN_ERROR_MARGIN = 200;
-export const SKILL_COOLDOWN_ERROR_MARGIN = 200;
-export const SPELL_COOLDOWN_ERROR_MARGIN = 200;
+// 쿨타임 허용 오차
+export const ATTACK_COOLDOWN_ERROR_MARGIN = 200; // 0.2초
+export const SKILL_COOLDOWN_ERROR_MARGIN = 200; // 0.2초
+export const SPELL_COOLDOWN_ERROR_MARGIN = 200; // 0.2초
 
+// 사정거리 허용 오차
 export const ATTACK_RANGE_ERROR_MARGIN = 1.05; // 5%
-export const SPEED_MARGIN_OF_ERROR = 1.05; // 속도 허용 오차배율
 
-export const INITIAL_UNIT_ROTATION = Object.freeze({ up: 0, down: 180 });
+// 속도 허용 오차
+export const SPEED_MARGIN_OF_ERROR = 1.05; // 5%
