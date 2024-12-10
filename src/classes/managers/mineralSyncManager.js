800e4e73e0ee35f16d2fb7dcc25b883d9e9db323
rlaguswls2 <hydrated200ml@gmail.com>
2024-12-10 13:04:04 +0900

    refactor: mineralSync jsDoc 작성 및 리팩토링
    Ref: #26

--- src/classes/managers/mineralSyncManager.js
+++ src/classes/managers/mineralSyncManager.js
@@ -7,30 +7,49 @@ import {
 import { PACKET_TYPE } from '../../constants/header.js';
 import { sendPacket } from '../../utils/packet/packetManager.js';
 
+/**
+ * 미네랄 동기화 클래스
+ * - 플레이어의 점령된 거점에 따라 미네랄을 동기화
+ * - 일정 주기로 미네랄을 갱신 및 클라이언트에 알림
+ */
 class MineralSyncManager {
   constructor() {
-    this.timer = null; // 단일 타이머
+    this.timer = null;
   }
 
-  // players -> playerGameData배열
+  /**
+   * 플레이어의 점령된 거점 수에 따른 미네랄 레이트 계산
+   * @param {number} checkpointCount - 점령된 거점 수
+   * @returns {number} - 계산된 미네랄 레이트
+   */
+  getMineralRate(checkpointCount) {
+    const mineralRateMap = {
+      0: INITIAL_MINERAL_RATE,
+      1: OCCUR_ONE_CHECKPOINT_MINERAL_RATE,
+      2: OCCUR_TWO_CHECKPOINT_MINERAL_RATE,
+    };
+    return mineralRateMap[checkpointCount] || INITIAL_MINERAL_RATE;
+  }
+
+  /**
+   * 미네랄 동기화 업데이트
+   * @param {Array<Object>} players - 플레이어 게임 데이터 배열
+   * @param {Object} checkpointManager - 체크포인트 매니저
+   */
   updateMineral(players, checkpointManager) {
     players.forEach((playerGameData) => {
-      const occurredCheckPoint = checkpointManager.getOccupiedCheckPointsByPlayer(playerGameData);
+      const checkpointCount = checkpointManager.getOccupiedCheckPointsByPlayer(playerGameData);
+      const mineralRate = this.getMineralRate(checkpointCount);
 
-      const mineralRateMap = {
-        0: INITIAL_MINERAL_RATE,
-        1: OCCUR_ONE_CHECKPOINT_MINERAL_RATE,
-        2: OCCUR_TWO_CHECKPOINT_MINERAL_RATE,
-      };
-
-      // 점령된 거점 수에 해당하는 미네랄 레이트 가져오기
-      const mineralRate = mineralRateMap[occurredCheckPoint] || INITIAL_MINERAL_RATE;
-
-      // 미네랄 적용
-      playerGameData.addMineral(mineralRate);
+      playerGameData.addMineral(mineralRate); // 미네랄 추가
     });
   }
 
+  /**
+   * 미네랄 동기화 루프 시작
+   * @param {Array<Object>} players - 플레이어 게임 데이터 배열
+   * @param {Object} checkpointManager - 체크포인트 매니저
+   */
   startSyncLoop(players, checkpointManager) {
     const interval = MINERAL_SYNC_INTERVAL;
 
