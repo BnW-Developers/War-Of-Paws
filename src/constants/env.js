dc54e1b46f5aade8e6c8230b933cb605d7e0616c
tjdcjf1996 <r_louis@naver.com>
2024-12-10 10:12:15 +0900

    remove: redis 관련 삭제
    Ref: #114

--- src/constants/env.js
+++ src/constants/env.js
@@ -20,12 +20,6 @@ export const DB2_PASSWORD = process.env.DB2_PASSWORD || '1q2w3e4r';
 export const DB2_HOST = process.env.DB2_HOST || 'localhost';
 export const DB2_PORT = process.env.DB2_PORT || 3306;
 
-/* ============================REDIS============================= */
-export const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
-export const REDIS_PORT = process.env.REDIS_PORT || 6379;
-export const REDIS_PASSWORD = process.env.REDIS_PASSWORD || '1q2w3e4r';
-export const REDIS_DATABASE = process.env.REDIS_DATABASE || 0;
-
 /* ============================ AUTH ============================ */
 export const PEPPER = process.env.PEPPER;
 export const SALT = process.env.SALT;
@@ -34,7 +28,3 @@ export const API_KEY = process.env.API_KEY;
 
 /* ============================ HOST ============================ */
 export const HEALTH_SERVER_HOST = process.env.HEALTH_SERVER_HOST;
-
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
