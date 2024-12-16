6728eee90a29e0d4cc52115be5345a506b592217
tjdcjf1996 <r_louis@naver.com>
2024-12-16 12:47:23 +0900

    refactor: 비정상 패킷 전송 시 아이피 블락
    비정상 패킷 전송 시 바로 아이피 차단 후 소켓연결 해제
    Ref: #68

--- src/utils/packet/packetManager.js
+++ src/utils/packet/packetManager.js
@@ -2,6 +2,7 @@ import PacketManager from '../../classes/managers/packetManager.js';
 import { PACKET_TYPE_REVERSED } from '../../constants/header.js';
 import { snakeToCamel } from '../formatter/snakeToCamel.js';
 import { createPacket } from '../response/createPacket.js';
+import { requestIpBan } from '../server/requestIpBan.js';
 
 const packetManager = new PacketManager();
 
@@ -16,7 +17,11 @@ export const sendPacket = (socket, type, payload = {}) => {
 };
 
 export const recvPacket = (socket, packet) => {
-  if (!socket || !packet) {
+  if (!packet) {
+    requestIpBan(socket.userIp, 'Transmission of non-compliant packets');
+    if (socket.authTimeout) clearTimeout(socket.authTimeout);
+    socket.end();
+    socket.destroy();
     throw new Error('receivePacket 인자값 확인하세요.');
   }
   packetManager.enQueueRecv(socket, packet);
