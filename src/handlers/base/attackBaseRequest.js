2b60a1134adb8d1eddc44758336dc28636db3944
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-12-17 21:40:21 +0900

    refactor: 성채공격 핸들러 기능분리
    - 성채공격 -> 성채공격 & 성채피격으로 기능분리
    
    Ref: #14

--- src/handlers/base/attackBaseRequest.js
+++ src/handlers/base/attackBaseRequest.js
@@ -1,4 +1,5 @@
 import { PACKET_TYPE } from '../../constants/header.js';
+import { baseLocation } from '../../utils/assets/getAssets.js';
 import { ERR_CODES } from '../../utils/error/errCodes.js';
 import { sendPacket } from '../../utils/packet/packetManager.js';
 import checkSessionInfo from '../../utils/sessions/checkSessionInfo.js';
@@ -6,38 +7,36 @@ import CustomErr from './../../utils/error/customErr.js';
 import { handleErr } from './../../utils/error/handlerErr.js';
 
 const attackBaseRequest = (socket, payload) => {
-  const { unitId } = payload;
-
   try {
-    const { userGameData, opponentGameData, gameSession } = checkSessionInfo(socket);
+    const { unitId } = payload;
+    const timestamp = Date.now();
 
-    if (!userGameData || !opponentGameData) {
-      throw new CustomErr(ERR_CODES.USER_NOT_FOUND, '유닛 정보를 불러오는데 실패하였습니다.');
-    }
+    const { userGameData, opponent, opponentSocket, gameSession } = checkSessionInfo(socket);
 
     const unit = userGameData.getUnit(unitId);
     if (!unit) throw new CustomErr(ERR_CODES.UNIT_NOT_FOUND, '유닛 정보를 찾을 수 없습니다.');
-    const damage = unit.getAttackPower();
+
+    // 쿨타임 검증
+    unit.checkAttackCooldown(timestamp);
+
+    unit.resetLastAttackTime(timestamp);
+
+    const species = opponent.getCurrentSpecies();
+    if (unit.isTargetOutOfRange(baseLocation[species])) {
+      throw new CustomErr(ERR_CODES.OUT_OF_RANGE, '대상 (성채)가 사거리 밖에 있습니다.');
+    }
 
     // 미점령 상태 성채 공격은 가능할 수 없음.
     const checkPointManager = gameSession.getCheckPointManager();
-    if (!checkPointManager.getCheckPointState(unitId))
+    if (!checkPointManager.getCheckPointState(unitId)) {
       throw new CustomErr(
         ERR_CODES.UNOCCUPIED_STATE_CHECKPOINT,
         '체크포인트가 점령되지 않은 상태에서는 공격할 수 없습니다.',
       );
-
-    const newBaseHp = opponentGameData.attackBase(damage);
-
-    if (newBaseHp <= 0) {
-      // 게임 종료 메서드 호출
-      gameSession.endGame();
-    } else {
-      [
-        { socket: userGameData.getSocket(), type: PACKET_TYPE.ATTACK_BASE_RESPONSE },
-        { socket: opponentGameData.getSocket(), type: PACKET_TYPE.BASE_ATTACKED_NOTIFICATION },
-      ].forEach(({ socket, type }) => sendPacket(socket, type, { baseHp: newBaseHp }));
     }
+
+    sendPacket(socket, PACKET_TYPE.ATTACK_BASE_RESPONSE, { unitId });
+    sendPacket(opponentSocket, PACKET_TYPE.ENEMY_ATTACK_BASE_NOTIFICATION, { unitId });
   } catch (err) {
     handleErr(socket, err);
   }
