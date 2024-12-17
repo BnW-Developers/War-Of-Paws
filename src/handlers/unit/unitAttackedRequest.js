3d26c3eac616f3fc417945136bff9ec123005bfd
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-12-17 21:39:41 +0900

    refactor: 유닛 공격 핸들러 기능분리
    - 유닛 공격 & 피격 핸들러 2개로 나누어 기능분리
    
    Ref: #11

---
+++ src/handlers/unit/unitAttackedRequest.js
@@ -0,0 +1,64 @@
+import { PACKET_TYPE } from '../../constants/header.js';
+import { handleErr } from '../../utils/error/handlerErr.js';
+import { sendPacket } from '../../utils/packet/packetManager.js';
+import checkSessionInfo from '../../utils/sessions/checkSessionInfo.js';
+
+/**
+ * 클라이언트로부터 데미지 요청을 수행하고 결과를 응답으로 전송
+ * @param {net.socket} socket
+ * @param {{ attackingUnitId: int32, attackedUnitId: int32 }} payload
+ */
+const unitAttackedRequest = (socket, payload) => {
+  try {
+    const { attackingUnitId, attackedUnitId } = payload;
+    const deathUnits = [];
+
+    const { userGameData, opponentGameData, opponentSocket, gameSession } =
+      checkSessionInfo(socket);
+
+    // unitId가 피공격자, opponent가 공격자
+    const attackedUnit = userGameData.getUnit(attackedUnitId);
+    const attackingUnit = opponentGameData.getUnit(attackingUnitId);
+
+    const resultHp = attackedUnit.applyDamage(attackingUnit.getAttackPower());
+
+    // 사망 체크
+    if (attackedUnit.getHp() <= 0) {
+      if (!attackedUnit.isDead()) {
+        attackedUnit.markAsDead();
+        const checkPointManager = gameSession.getCheckPointManager();
+        if (checkPointManager.isExistUnit(attackedUnit)) {
+          checkPointManager.removeUnit(attackedUnit);
+        }
+
+        userGameData.removeUnit(attackedUnitId);
+        deathUnits.push(attackedUnitId);
+      }
+    }
+
+    const unitInfo = {
+      unitId: attackedUnitId,
+      unitHp: resultHp,
+    };
+
+    sendPacket(socket, PACKET_TYPE.UNIT_ATTACKED_RESPONSE, {
+      unitInfo: unitInfo,
+    });
+
+    sendPacket(opponentSocket, PACKET_TYPE.ENEMY_UNIT_ATTACKED_NOTIFICATION, {
+      unitInfo: unitInfo,
+    });
+
+    sendPacket(socket, PACKET_TYPE.UNIT_DEATH_NOTIFICATION, {
+      unitIds: deathUnits,
+    });
+
+    sendPacket(socket, PACKET_TYPE.ENEMY_UNIT_DEATH_NOTIFICATION, {
+      unitIds: deathUnits,
+    });
+  } catch (error) {
+    handleErr(socket, error);
+  }
+};
+
+export default unitAttackedRequest;
