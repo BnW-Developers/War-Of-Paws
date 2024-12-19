194b8a1939c93d527a6b2ec9b1fa1f54ebb2d77c
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-12-19 17:30:08 +0900

    test, fix: 다른 유닛테스트를 진행할때 카드뽑기가 고장나는 현상 수정
    Ref: #73

--- src/test/util/updateContent.js
+++ src/test/util/updateContent.js
@@ -4,21 +4,15 @@ import { CURRENT_TEST, UNIT_TEST } from '../constants/testSwitch.js';
 const updateContent = (contents) => {
   switch (CURRENT_TEST) {
     case UNIT_TEST.DRAW_CARD: {
-      const content = contents.find(
-        (content) => content.packetType === PACKET_TYPE.DRAW_CARD_REQUEST,
-      );
-
-      const buttonType = Math.floor(Math.random() * 3) + 1;
-      content.payload = { buttonType };
+      // const content = contents.find(
+      //   (content) => content.packetType === PACKET_TYPE.DRAW_CARD_REQUEST,
+      // );
       break;
     }
     case UNIT_TEST.EXCEED_MAX_SLOTS: {
-      const content = contents.find(
-        (content) => content.packetType === PACKET_TYPE.DRAW_CARD_REQUEST,
-      );
-
-      const buttonType = Math.floor(Math.random() * 3) + 1;
-      content.payload = { buttonType };
+      // const content = contents.find(
+      //   (content) => content.packetType === PACKET_TYPE.DRAW_CARD_REQUEST,
+      // );
       break;
     }
     default: {
